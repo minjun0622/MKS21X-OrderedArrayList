a809c7d73d894df3b1845f7db1fd3c8313c39446
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-11-02 22:07:54 -0400

    This is the progress that was worked on in class. However, due to me not pulling, I was not able to push.

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,5 +1,38 @@
 import java.util.ArrayList;
 
 public class NoNullArrayList<T> extends ArrayList<T> {
-  
+public NoNullArrayList() {
+  super();
 }
+
+public NoNullArrayList (int startingCapacity) {
+  super (startingCapacity);
+}
+public T set (int index, T element) {
+  if (element == null) {
+    throw new IllegalArgumentException();
+  }
+  else {
+    return super.set(index, element);
+  }
+  }
+
+  public boolean add (T element) {
+    if (element == null) {
+      throw new IllegalArgumentException();
+    }
+    else {
+      return super.add(element);
+    }
+    }
+
+    public void add(int index, T element) {
+      if (element == null) {
+        throw new IllegalArgumentException();
+      }
+      else {
+        return super.add(index, element);
+      }
+      }
+
+    }
