5aca6ccd974b1c4ca10d3b524ff99073fa31f31e
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-11-03 21:40:53 -0400

    Added more.

---
+++ OrderedArrayList.java
@@ -0,0 +1,32 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  public OrderedArrayList() {
+    super();
+  }
+  public OrderedArrayList(int startingCapacity) {
+    super (startingCapacity);
+  }
+
+  public int get(T value) {
+    for (int i = 0; i < this.size; i++) {
+      if (value.compareTo(this.get(i)) <= 0)
+      return i;
+    }
+    return this.size();
+  }
+
+  public T set(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException();
+    }
+    this.remove(index);
+    this.add(element);
+    return element;
+  }
+
+  public void add(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException();
+    }
+    super.add(get(element), element);
+  }
+}
