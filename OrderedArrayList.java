8a0cf0bb696d035133d5ca7d7bc4c0d22aae94b3
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-11-05 00:43:50 -0500

    Cleaned up, realized we don't need to check nulls.

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -6,35 +6,23 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super (startingCapacity);
   }
 
-  public int get(T value) {
-    for (int i = 0; i < this.size; i++) {
-      if (value.compareTo(this.get(i)) <= 0)
-      return i;
-    }
-    return this.size();
-  }
-
 public boolean add(T element) {
-  if (element == null) {
-    throw new IllegalArgumentException();
+  for (int i = 0; i < this.size; i++) {
+    if (this.get(i)).compareTo(element) > 0; {
+      super.add(i, element);
+      return true;
+    }
   }
-  super.add(get(element) element);
-  return true;
+  return super.add(element);
 }
 
   public T set(int index, T element) {
-    if (element == null) {
-      throw new IllegalArgumentException();
-    }
     this.remove(index);
     this.add(element);
     return element;
   }
 
   public void add(int index, T element) {
-    if (element == null) {
-      throw new IllegalArgumentException();
-    }
-    super.add(get(element), element);
+    add(element);
   }
 }
